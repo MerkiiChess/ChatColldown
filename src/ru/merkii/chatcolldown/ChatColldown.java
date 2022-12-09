aef6cc9644d24e096e200b3de6a95548a5d2f97e
MerkiiChess <117251267+MerkiiChess@users.noreply.github.com>
2022-12-09 14:33:21 +0300

    Update ChatColldown.java

--- src/ru/merkii/chatcolldown/ChatColldown.java
+++ src/ru/merkii/chatcolldown/ChatColldown.java
@@ -6,8 +6,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 import ru.merkii.chatcolldown.listeners.ChatListener;
 
 public class ChatColldown extends JavaPlugin {
+	private static ChatCollDown instance;
 	public void onEnable() {
+		instance = this;
 		saveDefaultConfig();
 		Bukkit.getPluginManager().registerEvents(new ChatListener(this), this);
+	}
+	public static ChatCollDown getInstance() {
+		return instance;
 	}
 }
